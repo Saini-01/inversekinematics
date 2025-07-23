b20b1ddf86c0e72344b1f20c48b4fcf14e789e15
Aditya Saini <adityasaini145@gmail.com>
2025-07-23 22:03:01 +0530

    finish IK implementation

--- ik.c
+++ ik.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
 int lengthOne, lengthTwo, angleOne, angleTwo;
 
@@ -8,24 +10,49 @@ typedef struct{
     double angle;
 } arm;
 
-typedef struct {
+typedef struct{
     double x;
     double y;
 } point;
 
+typedef struct{
+    double t1;
+    double t2;
+} thetas;
+
 void setAngles(arm *armOne, arm *armTwo);
 void setLengths(arm *armOne, arm *armTwo);
 point computeEndEffector(arm *armOne, arm *armTwo);
+thetas solveIK(point target, arm *armOne, arm *armTwo);
 
 int main(int charc, char **charv){
     arm armOne, armTwo; 
-    point ee; 
+    point ee, target;
+    char bool[4];
+    thetas result;
 
     setLengths(&armOne, &armTwo);
     setAngles(&armOne, &armTwo);
 
     ee = computeEndEffector(&armOne, &armTwo);
     printf("The End Effector is located at: (%.2f, %.2f)\n", ee.x, ee.y);
+
+    printf("Do you have a specific point you want to reach? (y/n)");
+    scanf("%s", bool); 
+
+    printf("X value of target: ");
+    scanf("%lf", &target.x);
+    printf("Y value of target: ");
+    scanf("%lf", &target.y);
+
+    if(strcmp(bool, "y") == 0){
+        result = solveIK(target, &armOne, &armTwo);
+        printf("Value of theta for arm one: %.2f.\nValue of theta for arm two: %.2f.\n", result.t1, result.t2);
+    }
+    
+    else{
+        return 1;
+    }
 
     return 0;
 }
@@ -68,3 +95,32 @@ point computeEndEffector(arm *armOne, arm *armTwo){
     return ee;
 }
 
+thetas solveIK(point target, arm *armOne, arm *armTwo){
+    double x = target.x;
+    double y = target.y;
+    double L1 = armOne->length;
+    double L2 = armTwo->length;
+    double D = sqrt(x*x + y*y);
+    thetas t;
+
+    if (D > L1 + L2) {
+        printf("Target is not reachable.\n");
+        thetas errorTheta = { -1, -1 };
+        return errorTheta;
+    }
+
+    double cosTheta2 = (x*x + y*y - L1*L1 - L2*L2) / (2 * L1 * L2);
+    double theta2 = acos(cosTheta2); 
+
+    double k1 = L1 + L2 * cos(theta2);
+    double k2 = L2 * sin(theta2);
+    double theta1 = atan2(y, x) - atan2(k2, k1);
+
+    armOne->angle = theta1 * 180.0 / M_PI;
+    armTwo->angle = theta2 * 180.0 / M_PI;
+
+    t.t1 = armOne->angle;
+    t.t2 = armTwo->angle;
+
+    return t;
+}
